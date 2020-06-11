9aba8d49f56e6e92e4961d0563e7c725b2874ae2
MFAC <ninafroes@gmail.com>
2020-06-11 10:49:04 +0200

    feat: Add increment, decrement, init, clear functions

--- main.js
+++ main.js
@@ -3,32 +3,62 @@ const text = [
     name: "Tanya Sinclair",
     role: "UX Engineer",
     imageUrl: "./images/image-tanya.jpg",
-    text: `“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”`
+    text: `“I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.”`
   },
   {
     name: "John Tarkpor",
     role: "Junior Front-end Developer",
     imageUrl: "./images/image-john.jpg",
-    text: `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`
+    text: `“If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.”`
   }
 ];
 
+let counter = 0;
+
 function incrementCount() {
-  return console.log("increment");
+  if (counter < text.length) {
+    counter++;
+  } 
+
+  if (counter === text.length) {
+    counter = 0;
+  }
+  switchTestimonial()
 }
 
 function decrementCount() {
-  return console.log("decrement");
+  if (counter > 0) {
+    counter--;
+  } else {
+    counter = text.length - 1;
+  }
+
+  switchTestimonial()
 }
 
+function clear() {
+  const main = document.getElementById("app");
+  const testimonialContainer = document.querySelector(".testimonial-container");
+  main.removeChild(testimonialContainer);
+}
 
-(function showTestimonial(text) {
-  let counter = 0;
-  const testimonialContainer = document.getElementById('testimonial');
+function switchTestimonial() {
+  clear();
+  renderTestimonial();
+}
+
+function renderTestimonial() {
+  const main = document.getElementById("app");
+  const testimonialContainer = document.createElement("div");
   testimonialContainer.className = "testimonial-container";
   
   const textContainer = document.createElement("div");
+  textContainer.className = "text-container";
 
+  const quotePattern = document.createElement("img");
+  quotePattern.src = "./images/pattern-quotes.svg";
+  quotePattern.className = "quote-pattern";
+ 
   const userInfo = document.createElement("d");
   userInfo.className = "user-info";
 
@@ -50,7 +80,7 @@ function decrementCount() {
   const nextButton = document.createElement("button");
   nextButton.className = "next-button";
   nextButton.addEventListener("click", incrementCount);
-  
+
   const previousButton = document.createElement("button");
   previousButton.addEventListener("click", decrementCount);
   previousButton.className = "previous-button";
@@ -64,14 +94,24 @@ function decrementCount() {
   userImage.className = "user-image";
   userImage.src = text[counter].imageUrl;
 
+  const imageBtnContainer = document.createElement("div");
+  imageBtnContainer.className = "image-btn-container";
+
   imageContainer.append(userImage);
   imageContainer.append(buttonContainer);
+  imageBtnContainer.append(imageContainer);
   userInfo.append(userName);
   userInfo.append(userRole);
 
   textContainer.append(testimonialText);
   textContainer.append(userInfo);
+  textContainer.append(quotePattern);
 
   testimonialContainer.append(textContainer);
-  testimonialContainer.append(imageContainer);
-})(text);
+  testimonialContainer.append(imageBtnContainer);
+  main.append(testimonialContainer);
+};
+
+(function init() {
+  renderTestimonial()
+})();
